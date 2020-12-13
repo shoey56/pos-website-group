676d33a3ee7e054d12450ee8beaf7880457d96a7
Shannon Hoey <shannon.hoey@perficient.com>
2020-12-13 18:29:25 -0500

    Added credit card number validation

--- pos.js
+++ pos.js
@@ -200,3 +200,41 @@ function calculateTotal() {
     elem.innerHTML = `Total: $${total.toFixed(2)}`;
   }
 }
+
+//verify CC number
+document.getElementById("checkout-submit").addEventListener("click", validateCC);
+
+function validateCC(event){
+  event.preventDefault();
+  const number = document.getElementById("ccNumber").value
+  if (isCCValid(number)){
+    const validCC = document.querySelector(".receipt");
+    validCC.classList.toggle("hidden");
+    const checkout = document.querySelector(".checkout");
+  checkout.classList.toggle("hidden");
+  }else{
+    alert("Credit Card number is not valid. Please enter a valid credit number.")
+  }
+}
+
+function isCCValid(number) {
+  number = String(number);
+
+  let sum = parseInt(number.charAt(number.length - 1));
+
+  for (let i = 0; i < number.length - 1; i++) {
+      let value = parseInt(number.charAt(i));
+
+      if (i % 2 === 0) {
+          value *= 2;
+      }
+
+      if (value > 9) {
+          value -= 9;
+      }
+
+      sum += value;
+  }
+
+  return sum % 10 === 0;
+}
