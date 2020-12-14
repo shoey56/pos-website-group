5a12da3e94b427061e7ceb2ae621c2ba48afceb6
cherina brownlee <cherinabrownlee@amrock.com>
2020-12-14 14:07:48 -0500

    Adding descriptions

--- pos.js
+++ pos.js
@@ -1,3 +1,32 @@
+var modal = document.getElementById("myModal");
+
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
 //Set year min and max for age validation
 document.getElementById("year").max = new Date().getFullYear();
 document.getElementById("year").min = new Date().getFullYear() - 100;
@@ -18,3 +47,7 @@ function over21(event) {
     
   }
 }
+
+
+
+
